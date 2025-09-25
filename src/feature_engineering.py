2aa9fad8427bbad0e5b1acad23ad389c15de4282
and2049 <105667299+and2049@users.noreply.github.com>
2025-09-25 16:30:19 -0400

    completed feature engineering

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.feature_selection import VarianceThreshold
 from src.eda import retrieve_data, count_missing
 
 df = retrieve_data()
@@ -18,3 +21,18 @@ print(f"Original number of features: {X.shape[1]}")
 print(f"Number of features dropped: {len(columns_to_drop)}")
 print(f"Number of features remaining: {X_dropped.shape[1]}")
 print(count_missing(X_dropped))
+
+imputer = SimpleImputer(strategy="median", fill_value=-np.inf)
+X_imputed = imputer.fit_transform(X_dropped)
+X_imputed = pd.DataFrame(X_imputed, columns=X_dropped.columns)
+print(X_imputed.head())
+
+# removal of low var features
+selector = VarianceThreshold(threshold=0)
+X_reduced = selector.fit_transform(X_imputed)
+
+print(f"Original number of features: {X_imputed.shape[1]}")
+print(f"Features remaining after removing zero-variance features: {X_reduced.shape[1]}")
+
+def get_data():
+    return X_reduced, y
